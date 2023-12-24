e7e363990fa63c60f42db0b54e0ff22d00490b1c
Shahid Siddique <shahidsidd@gmail.com>
2023-12-24 04:25:19 -0600

    Reads the environment from user

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from automation.splunk_connector import read_config, connect_to_splunk
 
 def main():
 
-    config = read_config('config/environments.yml', 'DEV')
-    service = connect_to_splunk(config)
+    environment = input("Enter the environment name (e.g., 'DEV', 'PROD'): ")
+
+    try:
+        config = read_config('environments.yml', environment)
+        service = connect_to_splunk(config)
+    except FileNotFoundError:
+        print("Configuration file 'environments.yml' not found.")
+        sys.exit(1)
+    except KeyError:
+        print(f"Environment '{environment}' not found in the configuration.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        sys.exit(1)
 
     try:
         service = connect_to_splunk('path_to_your_config.yml', 'DEV')
